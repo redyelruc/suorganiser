9600c19be6f8df3b3efdc8b5e1476b080d7fffe1
aidan <redyelruc1@hotmail.com>
2020-04-21 18:02:22 +0100

    Updates the views file to access the database and fill the template accordingly. All working at the moment.

--- organizer/views.py
+++ organizer/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.template import loader
 from django.http.response import HttpResponse
 from .models import Tag
 
 def homepage(request):
     tag_list = Tag.objects.all()
-    output = ", ".join([tag.name for tag in tag_list])
+    template = loader.get_template('organizer/tag_list.html')
+    context = {'tag_list':tag_list}
+    output = template.render(context)
     return HttpResponse(output)
 
 # Create your views here.
